850189adbb2174f7b2c32d7cd6905a69cb132a20
Philip Ford <pford68@earthlink.net>
2015-05-30 20:14:59 -0400

    Removed the .ts extension for ILogger.

--- src/ILogger.js
+++ src/ILogger.js
@@ -1,4 +1,14 @@
 /**
  * For type-checking Loggers with TypeScript
  */
-//# sourceMappingURL=ILogger.js.map
+
+interface ILogger {
+    log(msg: string, ...varargs: any[]): void;
+    info(msg: string, ...varargs: any[]): void;
+    error(msg: string, ...varargs: any[]): void;
+    debug(msg: string, ...varargs: any[]): void;
+    warn(msg: string, ...varargs: any[]): void;
+    trace(msg: string, ...varargs: any[]): void;
+    assert(msg: string, ...varargs: any[]): void;
+}
+

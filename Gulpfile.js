9e5a66270a187c9ccb11e2fc345b8a5d685eee9a
Philip Ford <pford68@earthlink.net>
2017-07-13 01:33:41 -0400

    Added test dependnecies

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,65 @@
-/**
- *
- */
+'use strict';
 
 const gulp = require('gulp');
 const gutil = require('gulp-util');
 const jsdoc = require('gulp-jsdoc3');
+const jshint = require('gulp-jshint');
+const mocha = require('gulp-mocha');
+const istanbul = require('gulp-istanbul');
+const COVERAGE_THRESHOLD = 90;
+const src = ['./**/*.js', '!**/node_modules/**', '!Gulpfile.js'];
 
+
+//======================================================================== Tasks
 gulp.task('doc', function (cb) {
-    gulp.src(['README.md', './**/*.js', '!**/node_modules/**', '!Gulpfile.js'], {read: false})
+    let $src = src.unshift('README.md');
+    gulp.src($src, {read: false})
         .pipe(jsdoc(cb));
+});
+
+
+/*
+ Linting
+ */
+gulp.task('lint', () => {
+    return gulp.src(src)
+        .pipe(jshint('.jshintrc'))
+        // You can look into pretty reporters as well, but that's another story
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'));
+});
+
+
+/*
+ Sets up code coverage
+ */
+gulp.task('pre-test', () => {
+    return gulp.src(src)
+    // Covering files
+        .pipe(istanbul({includeUntested: true}))
+        // Force `require` to return covered files
+        .pipe(istanbul.hookRequire());
+});
+
+
+/*
+ Runs tests with coverage.
+ */
+gulp.task('test', ['pre-test'], () => {
+    return gulp.src('./test/**/*Spec.js')
+        .pipe(mocha({
+            reporter: 'list'
+        }))
+        .pipe(istanbul.writeReports())
+        // Enforce a coverage of at least 90%
+        .pipe(istanbul.enforceThresholds({ thresholds: { global: COVERAGE_THRESHOLD } }))
+        .on('error', gutil.log);
+});
+
+
+/*
+ Builds the entire project.
+ */
+gulp.task('default', ['lint', 'test'], done => {
+    done();
 });

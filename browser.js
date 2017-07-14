2f48eff8cba8b6bae9f2169decd615428be163b3
Philip Ford <pford68@earthlink.net>
2017-07-14 12:44:30 -0400

    No longer injected debugEnabled

--- browser.js
+++ browser.js
@@ -1,6 +1,17 @@
 /**
  *
  */
-module.exports = function inject(config){
-
+const fs = require('fs');
+const defaults = {
+    logger: './lib/loggers/BaseLogger',
+    appenders: 'BrowserConsoleAppender'
 };
+const extend = require('object-util').extend;
+let logConfig = {};
+
+if (fs.existsSync('./logProperties.json')){  // This should be in the CWD.
+   extend(logConfig, defaults, require('./logProperties.json'));
+}
+
+let logger = require(logConfig.logger)(logConfig);
+module.exports = require('./lib/slf4js')(logger, logConfig);
